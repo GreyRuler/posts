f661a4f21fa0454c216e455cb9bde4206d01a2f4
GreyRuler <daniil_vasilenko757@mail.ru>
2022-02-17 14:18:21 +0300

    Adding Property output

--- src/main/kotlin/Post.kt
+++ src/main/kotlin/Post.kt
@@ -4,9 +4,9 @@ import java.util.*
 data class Post(
     var id: String = "0",
     val ownerId: String = UUID.randomUUID().toString(),
-    var fromIs: Int,
-    var createdBy: Int,
-    val date: LocalDateTime = LocalDateTime.now(),
+    var fromIs: Int?,
+    var createdBy: Int?,
+    val date: String = LocalDateTime.now().toString(),
     var text: String,
     var replyOwnerId: Int,
     var replyPostId: Int,
@@ -24,9 +24,36 @@ data class Post(
     var isInteger: Int,
     var markedAsAds: Boolean,
     var isFavorite: Boolean,
-    var donut: Donut,
-    var postponedId: Int
-)
+    var donut: Donut?,
+    var postponedId: Int?
+) {
+    override fun toString(): String {
+        return "id: $id\n" +
+                "ownerId: $ownerId\n" +
+                "fromIs: $fromIs\n" +
+                "createdBy: $createdBy\n" +
+                "date: $date\n" +
+                "text: $text\n" +
+                "replyOwnerId: $replyOwnerId\n" +
+                "replyPostId: $replyPostId\n" +
+                "friendsOnly: $friendsOnly\n" +
+                "comments: $comments\n" +
+                "copyright: $copyright,\n" +
+                "likes: $likes\n" +
+                "reposts: $reposts\n" +
+                "views: $views\n" +
+                "postType: $postType\n" +
+                "signerId: $signerId\n" +
+                "canPin: $canPin\n" +
+                "canDelete: $canDelete\n" +
+                "canEdit: $canEdit\n" +
+                "isInteger: $isInteger\n" +
+                "markedAsAds: $markedAsAds\n" +
+                "isFavorite: $isFavorite\n" +
+                "donut: $donut\n" +
+                "postponedId: $postponedId"
+    }
+}
 
 data class Comment(
     var count: Int,
